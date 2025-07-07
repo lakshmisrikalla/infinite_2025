297dfe49f62f4342f1bcba48e4e0377cec48ba02
lakshmisrikalla <lakshmisrik@infinite.com>
2025-07-07 10:42:43 +0530

    Assignment 6

---
+++ Csharp/Assignment/Assignment 6/Count.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+    class Count
+        {
+            static void ReadStream()
+            {
+                FileStream fs = new FileStream("C:\\Project\\Csharp\\Assignment\\Assignment 6\\Assignment text file.txt" , FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                int count = 0;
+                string str = sr.ReadLine();
+                while (str != null)
+                {
+                    count++;
+                    Console.WriteLine(str);
+                    str = sr.ReadLine();
+                }
+                Console.WriteLine("Number of lines in file: {0}", count);
+                sr.Close();
+                fs.Close();
+            }
+            public static void Main()
+            {
+                ReadStream();
+                Console.Read();
+            }
+        }
+    }
---
+++ Csharp/Assignment/Assignment 6/CreateFiles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+
+    class CreateFiles
+    {
+        static void WriteStream(string[] lines)
+        {
+            FileStream fs = new FileStream("C:\\Project\\Csharp\\Assignment\\Assignment 6\\Assignment text file.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (var str in lines)
+            {
+                sw.WriteLine(str);
+            }
+            sw.Flush();
+            sw.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            Console.Write("Enter number of strings: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter string {0}", i + 1);
+                string s = Console.ReadLine();
+                lines.Add(s);
+            }
+            WriteStream(lines.ToArray());
+            Console.WriteLine("Strings successfully added in the file");
+            Console.Read();
+        }
+    }
+}
