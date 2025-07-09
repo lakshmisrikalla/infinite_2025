65246597a984199a9060b8074fb00cb5b604e6b9
lakshmisrikalla <lakshmisrik@infinite.com>
2025-07-09 10:58:23 +0530

    code challenge 3

--- Csharp/Code Challenge/Code Challenge 3/Calculator Q4.cs
+++ Csharp/Code Challenge/Code Challenge 3/Calculator Q4.cs
@@ -6,76 +6,49 @@ using System.Threading.Tasks;
 
 namespace Code_Challenge_3
 {
-    public class Calculator
-    {
-        
-        public delegate int Operation(int a, int b);
 
-        
-        public static int Execute(Operation op, int x, int y)
+        public delegate int CalculatorDelegate(int a, int b);
+
+        class Calculator_Q4
         {
-            return op(x, y);
-        }
-
-        
-        public static int Add(int a, int b)
-        {
-            return a + b;
-        }
-
-        
-        public static int Subtract(int a, int b)
-        {
-            return a - b;
-        }
-
-        
-        public static int Multiply(int a, int b)
-        {
-            int result = 0;
-            bool isNegative = false;
-
-            if (b < 0)
+            
+            public static int Add(int a, int b)
             {
-                b = -b;
-                isNegative = true;
+                return a + b;
             }
 
-            for (int i = 0; i < b; i++)
+            public static int Subtract(int a, int b)
             {
-                result += a;
+                return a - b;
             }
 
-            return isNegative ? -result : result;
-        }
-    }
-
-    public class Calculator_Q4
-    {
-        public static void Main()
-        {
-            Console.Write("Enter first number: ");
-            dynamic num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter second number: ");
-            dynamic num2 = Convert.ToInt32(Console.ReadLine());
+            public static int Multiply(int a, int b)
+            {
+                return a * b;
+            }
 
            
-            Calculator.Operation addOp = new Calculator.Operation(Calculator.Add);
-            Calculator.Operation subOp = new Calculator.Operation(Calculator.Subtract);
-            Calculator.Operation mulOp = new Calculator.Operation(Calculator.Multiply);
+            public static void PerformOperation(string operationName, CalculatorDelegate operation, int x, int y)
+            {
+                int result = operation(x, y);
+                Console.WriteLine($"{operationName} of {x} and {y} is: {result}");
+            }
 
-            
-            dynamic resultAdd = Calculator.Execute(addOp, num1, num2);
-            dynamic resultSub = Calculator.Execute(subOp, num1, num2);
-            dynamic resultMul = Calculator.Execute(mulOp, num1, num2);
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the first integer:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
 
-            
-            Console.WriteLine("\n--- Results ---");
-            Console.WriteLine("Addition Result: " + resultAdd);
-            Console.WriteLine("Subtraction Result: " + resultSub);
-            Console.WriteLine("Multiplication Result: " + resultMul);
-            Console.ReadLine();
+                Console.WriteLine("Enter the second integer:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                
+                PerformOperation("Addition", Add, num1, num2);
+                PerformOperation("Subtraction", Subtract, num1, num2);
+                PerformOperation("Multiplication", Multiply, num1, num2);
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
-}
